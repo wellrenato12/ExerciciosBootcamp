3ba12ae3349a2bc8d8128b58ade2ab9d882c6759
Wellington Albino <well.renato@hotmail.com>
2024-06-05 17:56:49 -0300

    Adicionando ex4 05/06

--- src/lacos_Condicionais/Ex4.java
+++ src/lacos_Condicionais/Ex4.java
@@ -8,13 +8,13 @@ public class Ex4 {
 		String palavra1, palavra2, palavra3;
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("[Vertebrado] [Invertebrado]");
+		System.out.println("Digite: [Vertebrado] ou [Invertebrado]");
 		palavra1 = sc.nextLine();
 
-		System.out.println("[Ave] [Mamífero] [Inseto] [Anelídeo]");
+		System.out.println("Digite: [Ave] ou [Mamífero] ou [Inseto] ou [Anelídeo]");
 		palavra2 = sc.nextLine();
 
-		System.out.println("[Carnívoro] [Onívoro] [Herbívoro] [Hematófago]");
+		System.out.println("Digite: [Carnívoro] ou [Onívoro] ou [Herbívoro] ou [Hematófago]");
 		palavra3 = sc.nextLine();
 
 		if (palavra1.equalsIgnoreCase("vertebrado")) {
@@ -25,8 +25,24 @@ public class Ex4 {
 					System.out.println("Pomba");
 				}
 			} else if (palavra2.equalsIgnoreCase("Mamífero")) {
-				if() {
-					
+				if(palavra3.equalsIgnoreCase("Onívoro")) {
+					System.out.println("Homem");
+				} else {
+					System.out.println("Vaca");
+				}
+			}
+		} else {
+			if(palavra2.equalsIgnoreCase("Inseto")) {
+				if(palavra3.equalsIgnoreCase("Hematófago")) {
+					System.out.println("Pulga");
+				} else {
+					System.out.println("Lagarta");
+				}
+			} else {
+				if(palavra3.equalsIgnoreCase("Hematófago")) {
+					System.out.println("Sanguessuga");
+				} else {
+					System.out.println("Minhoca");
 				}
 			}
 		}
