2bc70fbcfc0b2b88e131030b78da1c75ff48e24a
Wellington Albino <well.renato@hotmail.com>
2024-06-06 13:37:55 -0300

    ex6 06/06

--- src/LacosRepeticao/Ex6.java
+++ src/LacosRepeticao/Ex6.java
@@ -2,23 +2,27 @@ package LacosRepeticao;
 
 import java.util.Scanner;
 
-public class Ex5 {
+public class Ex6 {
 
 	public static void main(String[] args) {
-		int num, somaNumeros = 0;
+		int num, total = 0, i = 0;
+		float media = 0;
+		
 		Scanner sc = new Scanner(System.in);
 		
 		do {
 			System.out.println("Digite um número: ");
 			num = sc.nextInt();
-			if(num > 0) {
-				somaNumeros += num;
-			}
 			
+			if(num > 0 && num % 3 == 0) {
+				total += num;
+				i++;
+			}
 		} while (num != 0);
 		
-		System.out.println("A soma dos números positivos é: " + somaNumeros);
+		media = (float) total / i;
 		
+		System.out.printf("A média de todos os números múltiplos de 3 é: %.2f", media);
 	}
 
 }
