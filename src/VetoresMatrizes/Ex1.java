444401940c040dc362600ab9fbc4964b7c78015a
Wellington Albino <well.renato@hotmail.com>
2024-06-07 14:22:46 -0300

    ex1 07/06

---
+++ src/VetoresMatrizes/Ex1.java
@@ -0,0 +1,34 @@
+package VetoresMatrizes;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int[] numeros = new int[10];
+		int num = 0;
+		boolean encontrar = false;
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println("Digite um lista de números não repetidos: ");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		System.out.println("Digite um número para comparar: ");
+		num = scanner.nextInt();
+		
+		
+		for(int i = 0; i < numeros.length; i++) {
+			if(numeros[i] == num) {
+				encontrar = true;
+				System.out.println("O número " + num + " está localizado na posição: " + i);
+			}
+		}
+		
+		if(!encontrar) {
+			System.out.println("O número " + num + " não foi encontrado!");
+		}
+	}
+
+}
