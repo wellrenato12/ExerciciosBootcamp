88da8737d125a2d59a7157b6cdeebedf5e23ddf0
Wellington Albino <well.renato@hotmail.com>
2024-06-07 18:18:00 -0300

    melhorando ex1

--- src/VetoresMatrizes/Ex1.java
+++ src/VetoresMatrizes/Ex1.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		int[] numeros = new int[10];
+		int[] numeros = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
 		int num = 0;
 		boolean encontrar = false;
 		Scanner scanner = new Scanner(System.in);
 		
-		for(int i = 0; i < numeros.length; i++) {
+		/*for(int i = 0; i < numeros.length; i++) {
 			System.out.println("Digite um lista de números não repetidos: ");
 			numeros[i] = scanner.nextInt();
-		}
+		}*/
 		
 		System.out.println("Digite um número para comparar: ");
 		num = scanner.nextInt();
